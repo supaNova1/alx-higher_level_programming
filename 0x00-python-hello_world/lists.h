009c2a53711b384c87ee078bb781bbbca01d6a11
supaNova1 <anthonypeter1234@gmail.com>
2024-03-11 19:34:18 +0100

    linked

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -2,14 +2,17 @@
 #define LISTS_H
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * struct listint_s - singly linked list
  * @num: integer number
  * @next: points to the next node
  *
- * Description: singly linked list node structure
+ * Description: singly linked list node
  */
+
 typedef struct listint_s
 {
 	int num;
@@ -17,8 +20,10 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint(listint_t **head, const int num);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
+int check_cycle(listint_t *list);
 
-#endif /* LISTS_H */
+#endif
+/* LISTS_H */
