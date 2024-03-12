2f85f364fd7931b420753075d5b8fa7ed294afdc
supaNova1 <anthonypeter1234@gmail.com>
2024-03-12 20:00:40 +0100

    corrected

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# Loop through the ASCII values of lowercase letters 'a' to 'z'
-for i in range(ord('a'), ord('z') + 1):
-    if i != 'q' and i != 'e':
+for i in range(97, 123):
+    if (chr(i) != 'q' and chr(i) != 'e'):
         print("{:c}".format(i), end='')
