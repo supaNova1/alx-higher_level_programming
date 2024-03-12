a4067903163dfeccba092531072764c5e7e3069a
supaNova1 <anthonypeter1234@gmail.com>
2024-03-12 19:36:05 +0100

    retest

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 # Loop through the ASCII values of lowercase letters 'a' to 'z'
 for i in range(ord('a'), ord('z') + 1):
-    print(chr(i), end='')
+    print("{:c}".format(i), end='')
+
